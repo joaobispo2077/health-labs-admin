96ccd973840a02204f319b7bcd2d24386d54edcc
João Bispo <joaobispo2077@gmail.com>
2022-02-24 14:29:47 -0300

    feat: renderize resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Admin, ListGuesser, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useTheme } from './hooks/useTheme';
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+const App = () => {
+  const { selectedTheme } = useTheme();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <Admin theme={selectedTheme} dataProvider={dataProvider}>
+      <Resource name="users" list={ListGuesser} />
+      <Resource name="posts" list={ListGuesser} />
+      <Resource name="photos" list={ListGuesser} options={{ label: 'Fotos' }} />
+      <Resource name="todos" list={ListGuesser} />
+      {/* <Resource name="laboratories" list={LaboratoryList} /> */}
+    </Admin>
+  );
+};
 
-export default App
+export default App;
