dcaa3dae7461f3f8b710452adb23d89dc552d6ec
João Bispo <joaobispo2077@gmail.com>
2022-02-25 20:15:43 -0300

    feat: add exams list

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { Admin, ListGuesser, Resource } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import simpleRestProvider from 'ra-data-simple-rest';
 
 import { useTheme } from './hooks/useTheme';
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const dataProvider = simpleRestProvider('http://localhost:3333');
 
 const App = () => {
   const { selectedTheme } = useTheme();
 
   return (
     <Admin theme={selectedTheme} dataProvider={dataProvider}>
-      <Resource name="users" list={ListGuesser} />
+      {/* <Resource name="users" list={ListGuesser} />
       <Resource name="posts" list={ListGuesser} />
-      <Resource name="photos" list={ListGuesser} options={{ label: 'Fotos' }} />
       <Resource name="todos" list={ListGuesser} />
+      <Resource name="photos" list={ListGuesser} options={{ label: 'Fotos' }} /> */}
+      <Resource name="exams" list={ListGuesser} options={{ label: 'Exames' }} />
       {/* <Resource name="laboratories" list={LaboratoryList} /> */}
     </Admin>
   );
