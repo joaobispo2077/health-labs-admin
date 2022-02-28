d874f9949311c316987eac551962a3519365a633
João Bispo <joaobispo2077@gmail.com>
2022-02-28 14:35:22 -0300

    feat: render MobileExamList when user has a small device

--- src/components/Exams/ExamList.tsx
+++ src/components/Exams/ExamList.tsx
@@ -1,7 +1,15 @@
+import { ListGuesser } from "react-admin";
 import { ReactAdminFCList } from "../../@types/react-admin";
+import { useMediaQuery } from "../hooks/useMediaQuery";
 import { ExamModel } from "./Exam.model";
 import { MobileExamList } from "./MobileExamList";
 
 export const ExamList: ReactAdminFCList<ExamModel> = (props) => {
-  return <MobileExamList {...props}/>;
+  const isSmallDevice = useMediaQuery(["(max-width: 600px)"], [true], false);
+
+  if (isSmallDevice) {
+    return <MobileExamList {...props} />;
+  }
+
+  return <ListGuesser {...props}/>;
 }
