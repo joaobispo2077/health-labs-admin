4623aca0d40a56d93e5ff4c91bcbb5f997fb30e6
João Bispo <joaobispo2077@gmail.com>
2022-03-02 09:45:23 -0300

    refactor: rename custom react admin functional component type

--- src/@types/react-admin/index.ts
+++ src/@types/react-admin/index.ts
@@ -1,6 +1,8 @@
 import { ResourceComponentProps } from 'react-admin';
 
-export type ReactAdminFCList<GenericProps extends object = object> = (
+export type ReactAdminFunctionalComponent<
+  GenericProps extends object = object,
+> = (
   props: React.PropsWithChildren<
     ResourceComponentProps<GenericProps, unknown, unknown>
   >,
--- src/components/Exams/ExamList.tsx
+++ src/components/Exams/ExamList.tsx
@@ -1,10 +1,10 @@
 import { ListGuesser } from "react-admin";
-import { ReactAdminFCList } from "@src/@types/react-admin";
+import { ReactAdminFunctionalComponent } from "@src/@types/react-admin";
 import { useMediaQuery } from "@src/hooks/useMediaQuery";
 import { ExamModel } from "./Exam.model";
 import { MobileExamList } from "./MobileExamList";
 
-export const ExamList: ReactAdminFCList<ExamModel> = (props) => {
+export const ExamList: ReactAdminFunctionalComponent<ExamModel> = (props) => {
   const isSmallDevice = useMediaQuery(["(max-width: 600px)"], [true], false);
 
   if (isSmallDevice) {
